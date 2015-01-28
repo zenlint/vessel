ac5a09e7e82bcd8f709622e01656e7e9bce1a9dd
Unknwon <joe2010xtmf@163.com>
2015-01-28 07:44:46 -0500

    modules/flow: modifications on triggers

--- modules/flow/flow.go
+++ modules/flow/flow.go
@@ -1,24 +1,45 @@
 package flow
 
+// TODO: replace bool with a meaningful struct.
+type TriggerChan <-chan bool
+
 // Trigger is the interface that contains conditional channel to trigger pipeline or stage.
 type Trigger interface {
+	Trigger() error
+	SetReceiveChan(TriggerChan)
+}
+
+type Action interface {
 }
 
 // Stage represents a build process.
 type Stage struct {
 	Name string
 
-	beforeTrigger Trigger
-	afterTrigger  Trigger
+	receiveChan TriggerChan
+
+	onTrigger   Trigger
+	doneTrigger Trigger
+
+	beforeActions []Action
+	afterActions  []Action
+}
+
+// SetOnTrigger sets on trigger for stage.
+func (s *Stage) SetOnTrigger(t Trigger) {
+	t.SetReceiveChan(s.receiveChan)
+	s.onTrigger = t
 }
 
 // Pipeline represents a list of processes in order.
 type Pipeline struct {
 	Name string
 
-	stages         []*Stage
-	beforeTriggers []Trigger
-	afterTriggers  []Trigger
+	receiveChan TriggerChan
+
+	stages       []*Stage
+	onTriggers   []Trigger // TODO: should trigger all to make it happen
+	doneTriggers []Trigger
 }
 
 // AddStage adds a new stage to pipeline.
