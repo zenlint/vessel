8bce8c98421f5bb91c6374c81b9dbbc3c0f037ce
Meaglith Ma <maquanyi@huawei.com>
2015-10-29 13:39:09 +0000

    The newest version models definition

--- models/workspace.go
+++ models/workspace.go
@@ -9,7 +9,6 @@ type Workspace struct {
 	Name        string    `json:"name" orm:"varchar(255)"`
 	Description string    `json:"description" orm:"null;type(text)"`
 	Actived     bool      `json:"actived" orm:"null;default(0)"`
-	Locked      bool      `json:"locked" orm:"null;default(0)"`
 	Created     time.Time `json:"created" orm:"auto_now_add;type(datetime)"`
 	Updated     time.Time `json:"updated" orm:"auto_now;type(datetime)"`
 	Memo        string    `json:"memo" orm:"null;type(text)"`
