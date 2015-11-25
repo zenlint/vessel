76685130d38c9bd6eaffd2471e9d1e75e52b5396
Meaglith Ma <genedna@gmail.com>
2015-11-25 19:42:50 +0800

    Replace the redis with tidb

--- models/models.go
+++ models/models.go
@@ -1,8 +1,11 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"github.com/huawei-openlab/newdb/orm"
+	_ "github.com/pingcap/tidb"
+)
 
 func init() {
 	orm.RegisterModel(new(Workspace), new(Project), new(Pipeline), new(Status), new(Param), new(Point), new(Stage))
-	orm.RegisterDataBase("default", "tidb", "", 0, 0)
+	orm.RegisterDataBase("default", "tidb", "goleveldb:///tmp/tidb/vessel", 0, 0)
 }
