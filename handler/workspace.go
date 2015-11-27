6b7ad1b5e71446d9b2ef7168d16ae78481a2bab4
Meaglith Ma <genedna@gmail.com>
2015-11-27 17:38:27 +0800

    POST /v1/workspace

--- handler/workspace.go
+++ handler/workspace.go
@@ -1,16 +1,33 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/containerops/vessel/models"
+	"github.com/ngaut/log"
 	"gopkg.in/macaron.v1"
 )
 
 type WorkspacePOSTJSON struct {
+	Name        string `from:"name" binding:"Required"`
+	Description string `from:"description" binding:"Required"`
 }
 
 func V1POSTWorkspaceHandler(ctx *macaron.Context, workspace WorkspacePOSTJSON) (int, []byte) {
-	return http.StatusOK, []byte("")
+	w := models.Workspace{}
+
+	if id, err := w.Create(workspace.Name, workspace.Description); err != nil {
+		log.Errorf("[vessel] Create workspace error: %s", err.Error())
+
+		result, _ := json.Marshal(map[string]string{"status": "Error", "message": err.Error()})
+		return http.StatusBadRequest, result
+	} else {
+		log.Errorf("[vessel] Create workspace successfully, id is: %d", id)
+
+		result, _ := json.Marshal(map[string]int64{"id": id})
+		return http.StatusOK, result
+	}
 }
 
 func V1PUTWorkspaceHandler(ctx *macaron.Context) (int, []byte) {
--- models/workspace.go
+++ models/workspace.go
@@ -2,14 +2,41 @@ package models
 
 import (
 	"time"
+
+	"github.com/huawei-openlab/newdb/orm"
+	"github.com/ngaut/log"
 )
 
 type Workspace struct {
 	Id          int64     `json:"id"`
-	Name        string    `json:"name" orm:"varchar(255)"`
+	Name        string    `json:"name" orm:"unique;varchar(255)"`
 	Description string    `json:"description" orm:"null;type(text)"`
 	Actived     bool      `json:"actived" orm:"null;default(0)"`
 	Created     time.Time `json:"created" orm:"auto_now_add;type(datetime)"`
 	Updated     time.Time `json:"updated" orm:"auto_now;type(datetime)"`
 	Memo        string    `json:"memo" orm:"null;type(text)"`
+}
+
+func (workspace *Workspace) Create(name, description string) (int64, error) {
+	o := orm.NewOrm()
+	w := Workspace{Name: name, Description: description}
+
+	if err := o.Begin(); err != nil {
+		log.Errorf("Transcation error: %s", err.Error())
+		return 0, err
+	} else {
+		if id, e := o.Insert(&w); e != nil {
+			log.Errorf("Insert workspace data error: %s", e.Error())
+
+			o.Rollback()
+			return 0, e
+		} else {
+			log.Errorf("Insert worksapce successfully, id is: %d", id)
+
+			o.Commit()
+			return id, nil
+		}
+	}
+
+	return 0, nil
 }
