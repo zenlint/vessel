2d977b916484e5b61e45336ce2ca5e196faf8f8e
chinx <c54948054@126.com>
2016-06-06 13:35:51 +0800

    Setting file public function annotation

--- setting/setting.go
+++ setting/setting.go
@@ -19,50 +19,40 @@ type GlobalConf struct {
 
 // RunTimeConf
 type RunTimeConf struct {
-	Run      Run
-	Http     Http
-	Database Database
-	Etcd     Etcd
-	K8s      K8s
-}
-
-// Run
-type Run struct {
-	runMode string
-	logPath string
-}
-
-// Http
-type Http struct {
-	ListenMode    string
-	HttpsCertFile string
-	HttpsKeyFile  string
-	Host          string
-	Port          string
-}
-
-// Database
-type Database struct {
-	Username string
-	Password string
-	Protocol string
-	Host     string
-	Port     string
-	Schema   string
-	Param    map[string]string
-}
-
-// Etcd
-type Etcd struct {
-	Endpoints []map[string]string
-	Username  string
-	Password  string
-}
-
-// K8s
-type K8s struct {
-	Host string
-	Port string
+	// Run
+	Run struct {
+		runMode string
+		logPath string
+	}
+	// Http
+	Http struct {
+		ListenMode    string
+		HttpsCertFile string
+		HttpsKeyFile  string
+		Host          string
+		Port          string
+	}
+	// Database
+	Database struct {
+		Username string
+		Password string
+		Protocol string
+		Host     string
+		Port     string
+		Schema   string
+		Param    map[string]string
+	}
+	// Etcd
+	Etcd struct {
+		Endpoints []map[string]string
+		Username  string
+		Password  string
+	}
+	// K8s
+	K8s struct {
+		Host string
+		Port string
+	}
 }
 
 var (
