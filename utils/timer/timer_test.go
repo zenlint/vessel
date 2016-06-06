890a2024b1c570e9c35eaeabc1f2100a94776048
chinx <c54948054@126.com>
2016-06-06 17:46:07 +0800

    timer_test Errorf to LogF

--- utils/timer/timer_test.go
+++ utils/timer/timer_test.go
@@ -12,10 +12,10 @@ func TestLeftTime(t *testing.T) {
 	tim := InitHourglass(timeNanoSeconds)
 
 	<-time.After(time.Second * time.Duration(1))
-	t.Errorf("Testing timer.GetLeftTime for 1 sec err, TimeLeft: %d", tim.GetLeftNanoseconds())
+	t.Logf("Testing timer.GetLeftTime for 1 sec err, TimeLeft: %d", int64(tim.GetLeftNanoseconds()))
 
 	<-time.After(time.Second * time.Duration(2))
-	t.Errorf("Testing timer.GetLeftTime for 3 sec err, TimeLeft: %d", tim.GetLeftNanoseconds())
+	t.Logf("Testing timer.GetLeftTime for 3 sec err, TimeLeft: %d", int64(tim.GetLeftNanoseconds()))
 }
 
 // TestOverflow test over flow
@@ -24,6 +24,6 @@ func TestOverflow(t *testing.T) {
 	tim := InitHourglass(time.Second * time.Duration(initialTimeout))
 
 	<-time.After(time.Second * time.Duration(11))
-	t.Errorf("Testing timer.GetLeftTime overflow err, TimeLeft: %d", tim.GetLeftNanoseconds())
+	t.Logf("Testing timer.GetLeftTime overflow err, TimeLeft: %d", int64(tim.GetLeftNanoseconds()))
 
 }
