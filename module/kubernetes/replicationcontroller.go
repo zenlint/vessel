ad25f701a94c016dac822aadb6eced07ef8f26e8
chinx <c54948054@126.com>
2016-06-07 20:12:40 +0800

    kuerbetes module for next-generation

--- module/kubernetes/replicationcontroller.go
+++ module/kubernetes/replicationcontroller.go
@@ -1,1 +1,138 @@
 package kubernetes
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/containerops/vessel/models"
+	"github.com/containerops/vessel/utils/timer"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/labels"
+	"k8s.io/kubernetes/pkg/watch"
+)
+
+func createRC(stage *models.Stage) error {
+	if err := getClient(); err != nil {
+		return err
+	}
+	rc := &api.ReplicationController{
+		ObjectMeta: api.ObjectMeta{
+			Labels: map[string]string{},
+		},
+		Spec: api.ReplicationControllerSpec{
+			Template: &api.PodTemplateSpec{
+				ObjectMeta: api.ObjectMeta{
+					Labels: map[string]string{},
+				},
+			},
+			Selector: map[string]string{},
+		},
+	}
+	rc.Spec.Template.Spec.Containers = make([]api.Container, 1)
+	rc.SetName(stage.Name)
+	rc.SetNamespace(stage.Namespace)
+	rc.Labels[models.LabelKey] = stage.Name
+	rc.Spec.Replicas = int(stage.Replicas)
+	rc.Spec.Template.SetName(stage.Name)
+	rc.Spec.Template.Labels[models.LabelKey] = stage.Name
+	rc.Spec.Template.Spec.Containers[0] = api.Container{
+		Ports: []api.ContainerPort{
+			api.ContainerPort{
+				Name:          stage.Name,
+				ContainerPort: int(stage.Port),
+			},
+		},
+		Name:            stage.Name,
+		Image:           stage.Image,
+		ImagePullPolicy: "IfNotPresent",
+	}
+	if stage.EnvName != "" && stage.EnvValue != "" {
+		rc.Spec.Template.Spec.Containers[0].Env = []api.EnvVar{
+			api.EnvVar{
+				Name:  stage.EnvName,
+				Value: stage.EnvValue,
+			},
+		}
+	}
+	rc.Spec.Selector[models.LabelKey] = stage.Name
+	if _, err := k8sClient.ReplicationControllers(stage.Namespace).Create(rc); err != nil {
+		log.Println("Create rc err :", err)
+		return err
+	}
+	return nil
+}
+
+func deleteRC(stage *models.Stage) error {
+	if err := getClient(); err != nil {
+		return err
+	}
+	return k8sClient.ReplicationControllers(stage.Namespace).Delete(stage.Name)
+}
+
+func checkRC(stage *models.Stage) (bool, error) {
+	if err := getClient(); err != nil {
+		return false, err
+	}
+	rcs, err := k8sClient.ReplicationControllers(stage.Namespace).List(api.ListOptions{})
+	if err != nil {
+		return false, nil
+	}
+	for _, rc := range rcs.Items {
+		if rc.Name == stage.Name {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func watchRCStatus(stage *models.Stage, checkOp string, hourglass *timer.Hourglass, ch chan *models.K8sRes) {
+	if err := getClient(); err != nil {
+		ch <- formatResult(models.ResultFailed, err.Error())
+		return
+	}
+	if checkOp != string(watch.Added) && checkOp != string(watch.Deleted) {
+		ch <- formatResult(models.ResultFailed, fmt.Sprintf("Unexpected err when watch RC : name = %v", stage.Name))
+		return
+	}
+	if hourglass.GetLeftNanoseconds() <= 0 {
+		ch <- formatResult(models.ResultTimeout, fmt.Sprintf("Watch RC insterface timeout when name = %v", stage.Name))
+		return
+	}
+
+	opts := api.ListOptions{LabelSelector: labels.Set{models.LabelKey: stage.Name}.AsSelector()}
+	w, err := k8sClient.ReplicationControllers(stage.Namespace).Watch(opts)
+	if err != nil {
+		ch <- formatResult(models.ResultFailed, err.Error())
+		return
+	}
+	timeChan := time.After(time.Duration(hourglass.GetLeftNanoseconds()))
+	select {
+	case event, ok := <-w.ResultChan():
+		if !ok {
+			ch <- formatResult(models.ResultFailed, fmt.Sprintf("Unexpected err when watch RC : name = %v", stage.Name))
+			w.Stop()
+			return
+		}
+		if string(event.Type) == checkOp {
+			ch <- formatResult(models.ResultSuccess, "")
+			w.Stop()
+			return
+		}
+	case <-timeChan:
+		ch <- formatResult(models.ResultTimeout, fmt.Sprintf("Watch RC insterface timeout when name = %v", stage.Name))
+		w.Stop()
+		return
+	}
+}
+
+func getRCCount(stage *models.Stage) (int, error) {
+	if err := getClient(); err != nil {
+		return 0, err
+	}
+	rcs, err := k8sClient.ReplicationControllers(stage.Namespace).List(api.ListOptions{})
+	if err != nil {
+		return 0, err
+	}
+	return len(rcs.Items), nil
+}
--- module/kubernetes/service.go
+++ module/kubernetes/service.go
@@ -1,1 +1,87 @@
 package kubernetes
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/containerops/vessel/models"
+	"github.com/containerops/vessel/utils/timer"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/labels"
+	"k8s.io/kubernetes/pkg/util/intstr"
+	"k8s.io/kubernetes/pkg/watch"
+)
+
+func createService(stage *models.Stage) error {
+	if err := getClient(); err != nil {
+		return err
+	}
+	service := &api.Service{
+		ObjectMeta: api.ObjectMeta{
+			Labels: map[string]string{},
+		},
+		Spec: api.ServiceSpec{
+			Selector: map[string]string{},
+		},
+	}
+	service.Spec.Ports = make([]api.ServicePort, 1)
+	service.ObjectMeta.SetName(stage.Name)
+	service.ObjectMeta.SetNamespace(stage.Namespace)
+	service.ObjectMeta.Labels[models.LabelKey] = stage.Name
+	service.Spec.Ports[0] = api.ServicePort{Port: int(stage.Port), TargetPort: intstr.FromString(stage.Name)}
+	service.Spec.Selector[models.LabelKey] = stage.Name
+	if _, err := k8sClient.Services(stage.Namespace).Create(service); err != nil {
+		log.Println("Create service err :", err)
+		return err
+	}
+	return nil
+}
+
+func deleteService(stage *models.Stage) error {
+	if err := getClient(); err != nil {
+		return err
+	}
+	return k8sClient.Services(stage.Namespace).Delete(stage.Name)
+}
+
+func watchServiceStatus(stage *models.Stage, checkOp string, hourglass *timer.Hourglass, ch chan *models.K8sRes) {
+	if err := getClient(); err != nil {
+		ch <- formatResult(models.ResultFailed, err.Error())
+		return
+	}
+	if checkOp != string(watch.Added) && checkOp != string(watch.Deleted) {
+		ch <- formatResult(models.ResultFailed, fmt.Sprintf("Unexpected err when watch service : name = %v", stage.Name))
+		return
+	}
+	if hourglass.GetLeftNanoseconds() <= 0 {
+		ch <- formatResult(models.ResultTimeout, fmt.Sprintf("Watch service insterface timeout when name = %v", stage.Name))
+		return
+	}
+
+	opts := api.ListOptions{LabelSelector: labels.Set{models.LabelKey: stage.Name}.AsSelector()}
+	w, err := k8sClient.Services(stage.Namespace).Watch(opts)
+	if err != nil {
+		ch <- formatResult(models.ResultFailed, err.Error())
+		w.Stop()
+		return
+	}
+	timeChan := time.After(time.Duration(hourglass.GetLeftNanoseconds()))
+	select {
+	case event, ok := <-w.ResultChan():
+		if !ok {
+			ch <- formatResult(models.ResultFailed, fmt.Sprintf("Unexpected err when watch service : name = %v", stage.Name))
+			w.Stop()
+			return
+		}
+		if string(event.Type) == checkOp {
+			ch <- formatResult(models.ResultSuccess, "")
+			w.Stop()
+			return
+		}
+	case <-timeChan:
+		ch <- formatResult(models.ResultTimeout, fmt.Sprintf("Watch service insterface timeout when name = %v", stage.Name))
+		w.Stop()
+		return
+	}
+}
