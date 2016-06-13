4af00741c421351d46d8c742a054bafdc412b4dc
chinx <c54948054@126.com>
2016-06-13 16:48:36 +0800

    Add business health handler

---
+++ handler/health.go
@@ -0,0 +1,11 @@
+package handler
+
+import (
+	"gopkg.in/macaron.v1"
+	"net/http"
+)
+
+// V1GETHealth handler for HTTP GET
+func V1GETHealth(ctx *macaron.Context) (int, []byte) {
+	return http.StatusOK, []byte(0)
+}
